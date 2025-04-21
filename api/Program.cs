cb4ce5c601e7119d3f5f80c1712c4a7326fe289b
Uğur Nalbant <ugur.nalbant617@gmail.com>
2025-04-21 22:34:15 +0300

    StockController eklendi ve Program.cs dosyası güncellendi.(CRUD işlemlerine, RESTful API yapısına ve Model Binding kavramlarına giriş yapıldı.)

--- api/Program.cs
+++ api/Program.cs
@@ -5,6 +5,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddControllers();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -21,6 +24,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
+
 app.Run();
 
 
