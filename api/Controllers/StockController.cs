fe3029ed7119a10a0d6902e2bc9ecbdcae6274a4
Uğur Nalbant <ugur.nalbant617@gmail.com>
2025-04-26 14:36:31 +0300

    DTO yapısı(ve Mapper) ve StockController güncellemeleri eklendi.

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
+using api.Mappers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace api.Controllers
@@ -21,7 +22,8 @@ namespace api.Controllers
         [HttpGet]
         public IActionResult GetAll()
         {
-            var stock = _context.Stock.ToList();
+            var stock = _context.Stock.ToList().Select(s => s.toStockDto());;
+
             return Ok(stock);
         }
 
@@ -34,7 +36,7 @@ namespace api.Controllers
                 return NotFound();
             }
 
-            return Ok(stock);
+            return Ok(stock.toStockDto());
         }
     }
 }
